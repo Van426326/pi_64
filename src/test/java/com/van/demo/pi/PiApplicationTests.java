834a9d47dd13ca920a60cdd4453d61fa8ce68f86
gaof@chinacaring.com <FANRU@426>
2019-12-06 16:30:30 +0800

    first push

---
+++ src/test/java/com/van/demo/pi/PiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.van.demo.pi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
