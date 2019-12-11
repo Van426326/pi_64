4ac6031c9dad801d20eb0a412748a6c074bd05ab
Gaofan426 <762404743@qq.com>
2019-12-11 13:28:04 +0800

    跨域问题

--- src/main/java/com/van/demo/pi/config/CorsConfig.java
+++ src/main/java/com/van/demo/pi/config/CorsConfig.java
@@ -1,28 +1,18 @@
 package com.van.demo.pi.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-public class CorsConfig {
-    @Bean
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", buildConfig());
-        return new CorsFilter(source);
-    }
+public class CorsConfig extends WebMvcConfigurerAdapter {
 
-    private CorsConfiguration buildConfig() {
-        CorsConfiguration corsConfiguration = new CorsConfiguration();
-        // 1允许任何域名使用
-        corsConfiguration.addAllowedOrigin("*");
-        // 2允许任何头
-        corsConfiguration.addAllowedHeader("*");
-        // 3允许任何方法（post、get等）
-        corsConfiguration.addAllowedMethod("*");
-        return corsConfiguration;
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE")
+                .maxAge(3600)
+                .allowCredentials(true);
     }
 }
