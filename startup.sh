62f9e01c8c70c57058b8e49754d50aa409a48222
Gaofan426 <762404743@qq.com>
2019-12-11 13:58:36 +0800

    测试

--- startup.sh
+++ startup.sh
@@ -10,5 +10,5 @@ fi
 echo "pi64服务关闭成功"
 
 #执行jar，并将进程挂起，保存进程ID到 pid文件
-nohup java -Xms128m -Xmx128m -Xmn64m -jar -Duser.timezone=GMT+8 target/pi-0.0.1.jar --spring.profiles.active=$1 >/dev/null 2>&1 & echo "$!" > pid
+nohup java -Xms128m -Xmx128m -Xmn64m -jar -Duser.timezone=GMT+8 target/pi-0.0.1.jar --spring.profiles.active=$1 > log.log 2>&1 & echo "$!" > pid
 echo "pi64服务启动成功"
